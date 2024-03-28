6b020cc8fa2d81b48877795e531b943a634fbd18
An9ta <147629509+An9ta@users.noreply.github.com>
2024-03-28 23:46:24 +0100

    4

---
+++ js/task-05.js
@@ -0,0 +1,2 @@
+
+// Napisz skrypt, który przy wpisywaniu tekstu w polu input input#name-input (zdarzenie input) wstawia jego aktualną wartość do span#name-output. Jeśli pole input jest puste, w spanie powinien wyświetlić się tekst "Anonymous".
