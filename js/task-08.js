7ed0ded64693e3c5f14d8b8a2ed10467ca4fbeba
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-02 19:28:31 +0200

    9

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,37 @@
-const loginForm = document.querySelector("form.login-form");
-const inputs = document.querySelectorAll("form.login-form input");
+function checkForm(ev) {
+  const formData = {
+    email: null,
+    password: null,
+  };
 
-loginForm.addEventListener("submit", (ev) => {
+  const {
+    elements: { email, password },
+  } = ev.currentTarget;
+
   ev.preventDefault();
-  const form = ev.target;
-  const { elements } = form;
-  const inputsArr = Array.from(inputs);
-  if (!inputsArr.every((input) => input.value)) {
-    window.alert("Please fill in all fields before submitting the form.");
-  }
-});
 
-// if (login === "" || password === "") {
-//  return console.log("Please fill in all fields before submitting the form.");
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all fields before submitting the form.");
+  } else {
+    formData.email = email.value;
+    formData.password = password.value;
+    console.log(formData);
+    ev.currentTarget.reset();
+  }
+}
+
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", checkForm);
+
+//const loginForm = document.querySelector("form.login-form");
+//const inputs = document.querySelectorAll("form.login-form input");
+
+//loginForm.addEventListener("submit", (ev) => {
+//ev.preventDefault();
+//const form = ev.target;
+//const { elements } = form;
+//const inputsArr = Array.from(inputs);
+//if (!inputsArr.every((input) => input.value)) {
+//window.alert("Please fill in all fields before submitting the form.");
+//}
+//});
