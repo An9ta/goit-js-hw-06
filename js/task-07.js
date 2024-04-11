ec28afce349e5abcc9d5d0ecb9ceb5e175d31639
An9ta <147629509+An9ta@users.noreply.github.com>
2024-04-11 22:20:36 +0200

    12

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,8 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
+const initialSize = (text.style.fontSize = fontSizeControl.value + "px");
+
 fontSizeControl.addEventListener("input", () => {
   text.style.fontSize = `${fontSizeControl.value}px`;
 });
-
-const sliderControl = document.querySelector("#font-size-control");
-sliderControl.addEventListener("input", fontSizeControl);
